683aebbfee001d02df23fc856524a9986fa685f3
Markus Fenn <markus.fenn@evocortex.com>
2022-01-22 17:14:44 +0100

    Initial push MFE

---
+++ src/eduard_state_node.cpp
@@ -0,0 +1,104 @@
+//################################################################
+//# Copyright (C) 2022, EduArt Robotik GmbH, All rights reserved.#
+//# Further information can be found in the LICENSE file.        #
+//################################################################
+
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <unistd.h>
+
+enum MovementState{INIT, ENABLE, WAIT1, WAIT2, FORWARD, BACKWARD, TURNLEFT, TURNRIGHT};
+
+int main(int argc, char** argv)
+{
+     ros::init(argc, argv, "eduard_test");
+     ros:: NodeHandle nh = ros::NodeHandle("~");
+     ros::Publisher joy_pub = nh.advertise<sensor_msgs::Joy>("/joy", 10);
+
+     ros::Rate node_rate(10);
+
+    sensor_msgs::Joy joy_buf;
+    joy_buf.header.frame_id = "joy";
+    for(int i=0; i<4; i++)
+        joy_buf.axes.push_back(0.0);
+        
+    for(int i=0; i<11; i++)
+        joy_buf.buttons.push_back(0);
+
+    MovementState state = INIT;
+    ros::Time tRef = ros::Time::now();
+
+    while(ros::ok())
+    {
+        ros::Time t      = ros::Time::now();
+        ros::Duration dt = t - tRef;
+        double elapsed   = dt.toSec();
+
+        joy_buf.axes[0] = 0.0;
+        joy_buf.axes[1] = 0.0;
+        joy_buf.axes[2] = 0.0;
+        joy_buf.axes[3] = 0.0;
+        joy_buf.buttons[10] = 0;
+        switch(state)
+        {
+            case INIT:
+                // just send once the default message
+                if(elapsed > 1.0)
+                {
+                    state = ENABLE;
+                    tRef = t;
+                }
+            break;
+            case ENABLE:
+                // send enable signal
+                joy_buf.buttons[10] = 1;
+                if(elapsed > 1.0)
+                {
+                    state = WAIT1;
+                    tRef = t;
+                }
+            break;
+            case WAIT1:
+                // wait, while sending the default message
+                if(elapsed > 1.0)
+                {
+                    state = TURNLEFT;
+                    tRef = t;
+                }
+            break;
+            case TURNLEFT:
+                // turn robot left for a fixed period
+                joy_buf.axes[0] = 1.0;
+
+                if(elapsed > 1.0)
+                {
+                    state = WAIT2;
+                    tRef  = t;
+                }
+            break;
+            case WAIT2:
+                // wait, while sending the default message
+                if(elapsed > 1.0)
+                {
+                    state = TURNRIGHT;
+                    tRef = t;
+                }
+            break;
+            case TURNRIGHT:
+                // turn robot right for a fixed period
+                joy_buf.axes[0] = -1.0;
+
+                if(elapsed > 1.0)
+                {
+                    state = WAIT1;
+                    tRef  = t;
+                }
+            break;
+        }
+
+        joy_pub.publish(joy_buf);
+        node_rate.sleep();
+     }
+
+     return 0;
+}
---
+++ src/eduard_test_node.cpp
@@ -0,0 +1,83 @@
+//################################################################
+//# Copyright (C) 2022, EduArt Robotik GmbH, All rights reserved.#
+//# Further information can be found in the LICENSE file.        #
+//################################################################
+
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <unistd.h>
+
+#define VERSION1 0
+
+int main(int argc, char** argv)
+{
+     ros::init(argc, argv, "eduard_test");
+     ros:: NodeHandle nh = ros::NodeHandle("~");
+     ros::Publisher joy_pub = nh.advertise<sensor_msgs::Joy>("/joy", 10);
+
+     ros::Rate node_rate(10);
+     
+
+#if VERSION1
+     while(ros::ok())
+     {
+         
+        sensor_msgs::Joy joy_buf;
+        //Hi Stefan, bitte einmal zeigen, wie ich auf die Achsen und die Knöpfe schreibe,
+        //dann denke ich mir die Logic dazu aus (Switch case)
+        //Legst du für den Datenaustausch dann einfach ein Repo an?
+        //Ich brauche dann entsprechende Rechte auf dem Account "fennma67764"
+        joy_buf.header.frame_id = "joy";
+        joy_buf.axes.push_back(0.0);
+        joy_buf.axes.push_back(1.0);
+        joy_buf.axes.push_back(0.0);
+        joy_buf.axes.push_back(1.0);
+         
+        for(int i=0; i<9; i++)
+            joy_buf.buttons.push_back(0);
+        joy_buf.buttons.push_back(1);
+
+
+        // axes ist initial ein Vektor der Länge 0. Erst nachdem man mit push_back Elemente eingefügt hat, kann man auf diese mit den eckigen Klammern zugreifen.
+        // aber vorsicht! Du hast joy_buf innerhalb der while-Schleife definiert. Diese Variable wird am Ende der Schleife wieder aus dem Speicher entfernt und am Anfang der Schleifen neu angelegt.
+        // Du kannst also auch die push_back-Anweisungen einmal vor der while-Schleife ausführen und dann mit dem []-Operator lesend und schreibend zugreifen, siehe Version 2 unten.
+        //joy_buf.axes[0] = 0.0;
+        //joy_buf.axes[1] = 1.0;
+        //joy_buf.axes[2] = 0.0;
+        //joy_buf.axes[3] = 1.0;
+        //joy_buf.buttons[10] = 1;
+
+        joy_pub.publish(joy_buf);
+        ros::spinOnce();
+        node_rate.sleep();
+     }
+
+#else
+
+    sensor_msgs::Joy joy_buf;
+    joy_buf.header.frame_id = "joy";
+    for(int i=0; i<4; i++)
+        joy_buf.axes.push_back(0.0);
+        
+    for(int i=0; i<10; i++)
+        joy_buf.buttons.push_back(0);
+
+     while(ros::ok())
+     {
+         
+        joy_buf.axes[0] = 0.0;
+        joy_buf.axes[1] = 1.0;
+        joy_buf.axes[2] = 0.0;
+        joy_buf.axes[3] = 1.0;
+        joy_buf.buttons[10] = 1;
+
+        joy_pub.publish(joy_buf);
+        ros::spinOnce();
+        node_rate.sleep();
+     }
+
+#endif
+
+
+     return 0;
+}
